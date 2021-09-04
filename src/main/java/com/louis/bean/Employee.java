aa8b529a2d5cba70e902f4e275891b34231e4d72
louis_lai <1598358615@qq.com>
2021-09-04 12:08:22 +0800

    create bean

---
+++ src/main/java/com/louis/bean/Employee.java
@@ -0,0 +1,88 @@
+package com.louis.bean;
+
+/**
+ * @赖小燚
+ * @www.louis_lai.com
+ */
+public class Employee {
+
+    private Integer employeeId;
+    private String name;
+    private String email;
+    //1表示男性，0表示女性
+    private Integer gender;
+    private Integer departmentId;
+    private String departmentName;
+
+    public Employee(Integer employeeId, String name, String email, Integer gender, Integer departmentId, String departmentName) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+        this.departmentId = departmentId;
+        this.departmentName = departmentName;
+    }
+
+    public Employee() {
+    }
+
+    public Integer getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Integer employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public Integer getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", gender=" + gender +
+                ", departmentId=" + departmentId +
+                ", departmentName='" + departmentName + '\'' +
+                '}';
+    }
+}
